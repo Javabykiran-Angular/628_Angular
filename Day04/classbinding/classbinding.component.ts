e5bb0985ba731d6dda784ce7b62dc85b1565dbef
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-12 15:36:54 +0530

    Day04

---
+++ Day04/classbinding/classbinding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classbinding',
+  templateUrl: './classbinding.component.html',
+  styleUrls: ['./classbinding.component.css']
+})
+export class ClassbindingComponent {
+
+  myclass:string='success';
+  rating:number=2;
+}
