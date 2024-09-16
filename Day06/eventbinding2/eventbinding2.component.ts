456344c4fdfcac6062a6d83762350f3fd836138a
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-16 15:25:17 +0530

    Day06

---
+++ Day06/eventbinding2/eventbinding2.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbinding2',
+  templateUrl: './eventbinding2.component.html',
+  styleUrls: ['./eventbinding2.component.css']
+})
+export class Eventbinding2Component {
+result:number=0;
+name:string='';
+ 
+onAddition(num1:any,num2:any){
+
+let n1=+num1;
+let n2=+num2;
+// this.result=num1+num2;
+
+this.result=n1+n2;
+
+  }
+
+}
