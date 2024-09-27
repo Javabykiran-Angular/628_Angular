7f638f8b1893175e38e92cb35d6718d2a48c700e
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-27 15:59:09 +0530

    Day12

---
+++ Day12/post/post.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  constructor(private service:HttpService){ }
+  ngOnInit(): void {
+   this.getDataFromBackend();
+  }
+
+  getDataFromBackend(){
+    this.service.getData()
+    .subscribe((response)=>{
+      console.log(response);
+    })
+  }
+
+
+
+
+}
